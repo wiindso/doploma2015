cc035f5734fc0804bbbdb07b7ac20b9aa54d4d53
Sofiia Sorokina <sonik.sor@gmail.com>
2023-11-21 14:32:02 +0200

    add new line

--- Myfunc1.cpp
+++ Myfunc1.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+#include "windows.h"
+using namespace std;
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int n, m, s;
+    cout << "Введіть значення n: ";
+    cin >> n;
+    cout << "Введіть значення m: ";
+    cin >> m;
+    cout << "Введіть значення s: ";
+    cin >> s;
+    int resultC = (int)m * n + s;
+    cout << "Результат з використанням приведення типу в стилі мови С: " << resultC << endl;
+    return 0;
+}
